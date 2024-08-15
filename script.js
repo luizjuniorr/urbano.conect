72fe7db27c23ecb86a34c0d0dd73e5f3828e762f
luizjuniorr <luiz.fernando.junior@eaportal.org>
2024-08-15 08:19:13 -0300

    js conteudo

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ inicio.addEventListener("click", homeIr)
 function homeIr() {
     window.location.href = "./index.html"
 }
+
+const botaoConteudo = document.querySelector(".conteudo button")
+botaoConteudo.addEventListener("click", irConteudo)
+
+function irConteudo() {
+    window.location.href = "./pages/conteudo/conteudo.html"
+}
